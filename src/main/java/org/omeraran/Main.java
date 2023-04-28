1cb09e9af5ccce5b7fc420d53b1c4ce354501690
OmerAran <omerr.aran@gmail.com>
2023-04-28 14:35:09 +0300

    project initialized

---
+++ src/main/java/org/omeraran/Main.java
@@ -0,0 +1,9 @@
+package org.omeraran;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello world!");
+        }
+    }
+}
