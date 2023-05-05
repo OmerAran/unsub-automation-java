56be84a1179aece7e30bc800464f5cf568eea9d7
OmerAran <omerr.aran@gmail.com>
2023-05-05 23:04:00 +0300

    improvement tests works successfully for LinkedIn

--- src/main/java/org/omeraran/Main.java
+++ src/main/java/org/omeraran/Main.java
@@ -13,20 +13,30 @@ public class Main {
         String userAgents = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.137 Safari/537.36";
         options.addArguments(userAgents);
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent" +
-                ".com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth" +
-                ".com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3A1b8%2C16%3A751db3bd0c" +
-                "9f310a%2C10%3A1683153436%2C16%3A8a34ac06f5d2a2ee%2C6ded9aec0aadeca63d356015f77de881e8545c9a0ece0b7eef" +
-                "4ce55592a14939%22%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent" +
-                ".com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%220e999f56170842b187fc481efb540c9c%22%7D&response_type=" +
-                "code&service=lso&o2v=1&flowName=GeneralOAuthFlow");
-        driver.findElement(By.id("identifierId")).sendKeys("varartik63@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\'identifierNext\']/div/button/span")).click();
-        driver.findElement(By.name("password")).sendKeys("KARACA63m1w");
-        driver.findElement(By.xpath("//*[@id='passwordNext']/div/button/span")).click();
+        driver.get("https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fmynetwork%2Fnetwork-manager%2Fpeople-follow%2Ffollowers%2F&fromSignIn=true&trk=cold_join_sign_in");
+
+        driver.findElement(By.id("username")).sendKeys("mail@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("password");
+        //driver.findElement(By.xpath("//*[@class='btn__primary--large from__button--floating']")).click();
+        driver.findElement(By.xpath("//*[@class='btn__primary--large from__button--floating']")).click();
         Thread.sleep(1500);
-        driver.get("https://youtube.com/feed/channels");
-        driver.findElement(By.xpath("//*[@class='yt-spec-touch-feedback-shape__fill']")).click();
+        for (int i = 0; i < 50; i++) {
+            driver.get("https://www.linkedin.com/mynetwork/network-manager/people-follow/following/");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@data-control-name='entity_action_primary']")).click();
+            Thread.sleep(1500);
+            driver.findElement(By.xpath("//*[@class='artdeco-modal__confirm-dialog-btn artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")).click();
+            Thread.sleep(1000);
+            driver.get("https://www.linkedin.com/mynetwork/network-manager/people-follow/followers/");
+            Thread.sleep(2000);
+        }
+//        driver.get("https://www.linkedin.com/mynetwork/network-manager/people-follow/following/");
+//        Thread.sleep(1000);
+//        driver.findElement(By.xpath("//*[@data-control-name='entity_action_primary']")).click();
+//        Thread.sleep(1500);
+//        driver.findElement(By.xpath("//*[@class='artdeco-modal__confirm-dialog-btn artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")).click();
+//        Thread.sleep(1000);
+//        driver.get("https://www.linkedin.com/mynetwork/network-manager/people-follow/following/");
         driver.quit();
 
     }
